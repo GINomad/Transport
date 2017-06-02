4ca4bd52f56597d355b1f09b04a17e2847d09646
Alexander Kruk <kruksasha@gmail.com>
2017-06-03 01:06:30 +0300

    Init project with db

---
+++ Transport.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Transport.WEB.Startup))]
+namespace Transport.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
